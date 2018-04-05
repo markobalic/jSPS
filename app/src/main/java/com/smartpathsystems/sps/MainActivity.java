438e5a3cff0933b4495724042e5aa824b6a9113a
Marko Balic <32818612+markobalic@users.noreply.github.com>
2018-04-06 00:12:35 +0200

    added maps, current location, welcome screen, app icon

--- app/src/main/java/com/smartpathsystems/sps/MainActivity.java
+++ app/src/main/java/com/smartpathsystems/sps/MainActivity.java
@@ -1,15 +1,67 @@
 package com.smartpathsystems.sps;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng));
+            }
+        });
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(MainActivity.this, "Ne upiri pinpoint", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Ako lokacije nisu dopustene, salje se prompt window sa pitanjem da se dopuste
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 5);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 6);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                    Manifest.permission.INTERNET
+            }, 7);
+            mMap.setMyLocationEnabled(true);
+        }
+        else
+            mMap.setMyLocationEnabled(true);
     }
 }
